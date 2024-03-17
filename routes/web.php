e7014e83493113cc2f21ea2540babe4a322d5ebb
Randy Sum <randy26smc@gmail.com>
2024-03-17 02:43:16 -0600

    implementacion de roles con laravel permission, se implento el rol de admin, vendedor, comprador, voluntario

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Permission\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('{any}', function(){
+//Creamos roles
+#$role = Role::create(['name' => 'admin']);
+#$role = Role::create(['name' => 'vendedor']);
+#$role = Role::create(['name' => 'comprador']);
+#$role = Role::create(['name' => 'voluntario']);
+
+
+Route::get('{any}', function () {
     return view('welcome');
 })->where('any', '.*');
