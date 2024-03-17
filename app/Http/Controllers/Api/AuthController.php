d5215dcaee782ac646457d8485041605b81efd36
Randy Sum <randy26smc@gmail.com>
2024-03-17 13:24:56 -0600

    implementacion de login desde rest api, recuperación de token

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-
+        $response = ["success" => false];
         //validacion
 
         $validator = Validator::make($request->all(), [
@@ -21,12 +21,10 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             $response = ["error" => $validator->errors()];
-            return response()->json($response,200);
+            return response()->json($response, 200);
         }
-
-        $response = ["success" => "false"];
 
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
@@ -37,7 +35,39 @@ class AuthController extends Controller
 
 
         $response["success"] = "true";
-        //$response["token"] = $user->createToken("nacho")->plainTextToken;
+        $response["token"] = $user->createToken("nacho")->plainTextToken;
+
+        return response()->json($response, 200);
+    }
+
+    public function login(Request $request)
+    {
+
+        $response = ["success" => false];
+
+        //validacion
+        $validator = Validator::make($request->all(), [
+            //'name'=>'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $response = ["error" => $validator->errors()];
+            return response()->json($response, 200);
+        }
+
+        //autenticación
+        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = auth()->user();
+            $user->hasRole('comprador');
+            //Envia token al frontedn
+            $response["token"] = $user->createToken("nacho")->plainTextToken;
+
+            //envia al usuario
+            $response['user'] = $user;
+            $response['success'] = true;
+        }
 
         return response()->json($response, 200);
     }
