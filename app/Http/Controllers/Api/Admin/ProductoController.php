8d832e92f32cb11e838afbf0355b2d2770cb382f
Randy Sum <randy26smc@gmail.com>
2024-03-18 13:13:01 -0600

    CRUD para los productos desde api

--- app/Http/Controllers/Api/Admin/ProductoController.php
+++ app/Http/Controllers/Api/Admin/ProductoController.php
@@ -3,9 +3,74 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductoController extends Controller
 {
-    //
+    public function index(){
+        $data = Producto::orderBy("orden")->get(['id', 'nombre', 'precio']);
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request){
+        //validación
+        $data = new Producto(($request->all()));
+
+        if ($request->urlfoto) {
+            $img = $request->urlfoto;
+            //process
+            $folderPath = "/img/producto/";
+            $image_parts = explode(";base64,", $img);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+            $image_base64 = base64_decode($image_parts[1]);
+            $file = $folderPath . Str::slug($request->nombre) . '.' . $image_type;
+            file_put_contents(public_path($file), $image_base64);
+
+            $data->urlfoto = Str::slug($request->nombre) . '.' . $image_type;
+        }
+
+        $data->save();
+        return response()->json($data,200);
+
+
+
+    }
+
+    public function show($id){
+        $data = Producto::find($id);
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id){
+        $data = Producto::find($id);
+        $data->fill($request->all());
+
+        if ($request->urlfoto) {
+            $img = $request->urlfoto;
+            //process
+            $folderPath = "/img/producto/";
+            $image_parts = explode(";base64,", $img);
+            $image_type_aux = explode("image/", $image_parts[0]);
+            $image_type = $image_type_aux[1];
+            $image_base64 = base64_decode($image_parts[1]);
+            $file = $folderPath . Str::slug($request->nombre) . '.' . $image_type;
+            file_put_contents(public_path($file), $image_base64);
+
+            $data->urlfoto = Str::slug($request->nombre) . '.' . $image_type;
+        }
+
+        $data->save();
+        return response()->json($data,200);
+
+    }
+
+    public function destroy($id){
+        $data = Producto::find($id);
+        $data->delete();
+        return response()->json("Producto eliminado correctamente",200);
+
+    }
 }
--- app/Http/Controllers/Api/Vendedor/ServicioController.php
+++ app/Http/Controllers/Api/Vendedor/ServicioController.php
@@ -8,4 +8,24 @@ use Illuminate\Http\Request;
 class ServicioController extends Controller
 {
     //
+    public function index(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+        
+    }
+}
 }
