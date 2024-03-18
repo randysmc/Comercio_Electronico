ff8248061c633f6d5629c9bf491174e130f80a88
Randy Sum <randy26smc@gmail.com>
2024-03-18 03:09:36 -0600

    desde api se listan los usuarios obteniendo todos, solo uno, y se actualiza un usuario

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //Me muestra todos los usuarios
+    public function index(){
+        $data = User::all();
+        return response()->json($data, 200);
+    }
+
+    //Me muestra solo un usuario por ID
+    public function show($id){
+        $data = User::find($id);
+        return response()->json($data, 200);
+
+    }
+
+    //para actualizar
+    public function update(Request $request, $id){
+        //validación de datos
+
+        $data = User::find($id);
+        $data-> fill($request->all());
+        $data-> save();
+
+        return response()->json($data, 200);
+    }
 }
